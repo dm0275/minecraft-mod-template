8ec37245a4c07b6162028c29737ce273aee81db6
Daniel Mancilla <dm0275@gmail.com>
2023-01-07 23:19:43 -0600

    Added new resource pkgs

--- src/main/java/com/fussionlabs/minecraft/CommonProxy.java
+++ src/main/java/com/fussionlabs/minecraft/CommonProxy.java
@@ -1,12 +1,25 @@
 package com.fussionlabs.minecraft;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public class CommonProxy {
+
+    @SubscribeEvent
+    public static void registerRenders(ModelRegistryEvent event) {
+        registerRender(SwordMod.sword);
+    }
+
+    public static void registerRender(Item item) {
+        ModelLoader.setCustomModelResourceLocation(item, 0,
+                new ModelResourceLocation(item.getRegistryName(), "inventory"));
+    }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
